95d97a106249db8c8c1e1e169a9c70cda691878e
Vincent Gabriel <vadimg88@gmail.com>
2017-06-29 15:39:37 -0700

    es5 changes.

--- src/utils/popoverMixins.js
+++ src/utils/popoverMixins.js
@@ -59,7 +59,7 @@ export default {
     }
   },
   mounted () {
-    let trigger = this.$refs.trigger.children[0]
+    var trigger = this.$refs.trigger.children[0]
     if (!trigger) return console.error('Could not find trigger v-el in your component that uses popoverMixin.')
 
     if (this.trigger === 'focus' && !~trigger.tabIndex) {
